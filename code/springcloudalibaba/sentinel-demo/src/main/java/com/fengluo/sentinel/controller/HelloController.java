9acc501147e98b1420ec028abd1d2b06af57bd0b
FengLuo97 <3167881811@qq.com>
2022-07-24 21:45:28 +0800

    【ADD】SpringCloudAlibaba学习

--- code/springcloudalibaba/sentinel-demo/src/main/java/com/fengluo/sentinel/controller/HelloController.java
+++ code/springcloudalibaba/sentinel-demo/src/main/java/com/fengluo/sentinel/controller/HelloController.java
@@ -3,11 +3,14 @@ package com.fengluo.sentinel.controller;
 import com.alibaba.csp.sentinel.Entry;
 import com.alibaba.csp.sentinel.SphU;
 import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.Rule;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.fengluo.sentinel.pojo.User;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.tools.Trace;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +30,29 @@ public class HelloController {
     private static final String USER_RESOURCE_NAME = "user";
     private static final String DEGRADE_RESOURCE_NAME = "degrade";
 
+    @PostConstruct
+    private static void initFlowRules() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        // 设置要保护的资源
+        rule.setResource(USER_RESOURCE_NAME);
+        // 设置流控规则 QPS
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        //设置受保护的资源阈值
+        rule.setCount(1);
+        rules.add(rule);
+        // 加载配置好的规则
+        FlowRuleManager.loadRules(rules);
+    }
 
+    @RequestMapping("/user")
+    @SentinelResource(value = USER_RESOURCE_NAME, blockHandler = "blockHandlerForGetUser")
+    public User getUser(String id) {
+        return new User("fengluo");
+    }
+
+    public User blockHandlerForGetUser(String id, BlockException e) {
+        e.printStackTrace();
+        return new User("流控！");
+    }
 }
--- code/springcloudalibaba/sentinel-demo/src/main/java/com/fengluo/sentinel/pojo/User.java
+++ code/springcloudalibaba/sentinel-demo/src/main/java/com/fengluo/sentinel/pojo/User.java
@@ -1,8 +1,18 @@
 package com.fengluo.sentinel.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @Author: fengluo
  * @Date: 2022/7/23 23:05
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    private String name;
+
 }
